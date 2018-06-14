dedb5aa3baff9990ff168edb623448c28477effc
Smolinskie <edward_smolinski@comcast.net>
2018-06-13 20:12:33 -0500

    Updates to the Assign task screen connecting  the GUI fields to the Entity framerwork.

--- devTeamScheduler/devTeamScheduler/AssignTasks.xaml.cs
+++ devTeamScheduler/devTeamScheduler/AssignTasks.xaml.cs
@@ -2,6 +2,7 @@
 using System.Windows;
 using System.Windows.Controls;
 using System.Linq;
+using System;
 
 namespace devTeamScheduler
 {
@@ -15,10 +16,14 @@ namespace devTeamScheduler
             InitializeComponent();
             Model model = new Model();
 
-            foreach (var item in model.Users)
-            {
-                UserDropDown.Items.Add(item.fName + " " + item.lName);
-            }
+            var users = (from u in model.Users select new { u.UID, name = u.lName + ", " + u.fName }).ToList();
+
+            // set the display and value members for the combo box
+            UserDropDown.DisplayMemberPath = "name";
+            UserDropDown.SelectedValuePath = "UID";
+
+            // now set the data source
+            UserDropDown.ItemsSource = users;
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
@@ -33,25 +38,56 @@ namespace devTeamScheduler
             else
                 return;
 
+        
+            /*
+        private void CheckFields()
+        {
+
+           
+            String[] textBoxes = { TaskTextBox.Text, DevBranchTextBox.Text, TaskDescriptionTextBox.Text };
+
+            if (textBoxes.ToString.isNullOrWhiteSpace)
+            {
+                MessageBox.Show("Hi");
+            }
+
         }
+        */
 
         private void Button_Click_1(object sender, RoutedEventArgs e)
         {
             //create a new instance of model
             Model model = new Model();
 
-            if (MessageBox.Show("Would you like to submit the following changes to user" + UserDropDown.SelectedItem, "Submit Changes", MessageBoxButton.YesNo) == MessageBoxResult.Yes)
+            //prompt the user with a message box to confirm they want the changes submitted.
+            if (MessageBox.Show("Would you like to submit the following changes to user " + UserDropDown.SelectedItem, "Submit Changes", MessageBoxButton.YesNo) == MessageBoxResult.Yes)
             {
-                //prompt the user with a message box to confirm they want the changes submitted.             
-                Entry entry = new Entry();
-                entry.taskName = TaskTextBox.Text;
-                //save the changes to the task database
+                //create a new instance of Task to save the data to the task table                
+                Task task = new Task();
+
+                task.taskName = TaskTextBox.Text;
+                task.UID = (int)UserDropDown.SelectedValue;
+                task.devBranch = DevBranchTextBox.Text;
+                task.longDesc = TaskDescriptionTextBox.Text;
+                task.dateDue = AssignDueDate.SelectedDate.Value;
+                //gets the current date and time the submit was generated
+                task.dateCreated = DateTime.Now;
+                task.completed = false;
+
+                //add the data to the task table
+                model.Tasks.Add(task);
+                //save changes 
                 model.SaveChanges();
             }
             else
             {
                 return;
             }
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            CheckFields();
         }
     }
 }
