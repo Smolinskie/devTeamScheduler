815b67d44197e1c7ee90f9c773cbb5577d7d340a
Daniel Baker <danbaker951@gmail.com>
2018-06-18 18:20:04 -0500

    - Fixed tab order on login screen - started integrating a tab control on the main window

--- devTeamScheduler/devTeamScheduler/MyTasks.xaml.cs
+++ devTeamScheduler/devTeamScheduler/MyTasks.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System.Windows;
+using System.Windows.Controls;
 
 namespace devTeamScheduler
 {
@@ -16,9 +17,14 @@ namespace devTeamScheduler
 
         private void Button_Click_1(object sender, RoutedEventArgs e)
         {
-            var openSelectedTask = new SelectedTask(1);
-            openSelectedTask.Show();
-            this.Close();
+            // add a new SelectedTask page to the tab control base on code from: https://stackoverflow.com/questions/15589298/how-to-add-wpf-page-to-tabcontrol            
+            TabItem tabitem = new TabItem();
+            tabitem.Header = "Selected Task";
+            Frame tabFrame = new Frame();
+            var openSelectedTask = new SelectedTaskPage(1);
+            tabFrame.Content = openSelectedTask;
+            tabitem.Content = tabFrame;
+            TabController.Items.Add(tabitem);
         }
 
         private void Button_Click_2(object sender, RoutedEventArgs e)
